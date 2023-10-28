d0621e24ac2c2cfa7a28318e410fe2b4e939e376
DerAnderDo <113915726+DerAnderDo@users.noreply.github.com>
2023-10-28 16:38:56 +0200

    Update bh1750_reader.ino

--- src/bh1750_reader.ino
+++ src/bh1750_reader.ino
@@ -1,1 +1,41 @@
+/*
 
+Modified example of BH1750 library usage.
+
+This example initialises the BH1750 object using the default high resolution
+continuous mode and then makes a light level reading every second.
+
+Connections
+
+  - VCC to 3V3 or 5V
+  - GND to GND
+  - SCL and SDA as in this example pin 22 and 21 of the TTGO T7 V1.4
+  - ADD to (not connected) or GND
+
+ADD pin is used to set sensor I2C address. If it has voltage greater or equal
+to 0.7VCC voltage (e.g. you've connected it to VCC) the sensor address will be
+0x5C. In other case (if ADD voltage less than 0.7 * VCC) the sensor address
+will be 0x23 (by default).
+
+*/
+
+#include <BH1750.h>
+#include <Wire.h>
+
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  Wire.begin(22, 21);
+  lightMeter.begin();
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+}
