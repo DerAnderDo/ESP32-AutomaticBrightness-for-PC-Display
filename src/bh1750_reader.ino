db37f1e9a695af62f9a22ea9bf36682ba98626ec
DerAnderDo <113915726+DerAnderDo@users.noreply.github.com>
2023-11-26 17:54:50 +0100

    Arduino shit bug

--- src/bh1750_reader.ino
+++ src/bh1750_reader.ino
@@ -1,24 +1,3 @@
-/*
-
-Modified example of BH1750 library usage.
-
-This example initialises the BH1750 object using the default high resolution
-continuous mode and then makes a light level reading every second.
-
-Connections
-
-  - VCC to 3V3 or 5V
-  - GND to GND
-  - SCL and SDA as in this example pin 22 and 21 of the TTGO T7 V1.4
-  - ADD to (not connected) or GND
-
-ADD pin is used to set sensor I2C address. If it has voltage greater or equal
-to 0.7VCC voltage (e.g. you've connected it to VCC) the sensor address will be
-0x5C. In other case (if ADD voltage less than 0.7 * VCC) the sensor address
-will be 0x23 (by default).
-
-*/
-
 #include <BH1750.h>
 #include <Wire.h>
 
@@ -26,16 +5,17 @@ BH1750 lightMeter;
 
 void setup() {
   Serial.begin(9600);
-
+  delay(100);
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin(22, 21);
   lightMeter.begin();
+  delay(100);
 }
 
 void loop() {
   float lux = lightMeter.readLightLevel();
-  Serial.print("L: ");
-  Serial.print(lux);
-  Serial.println(" lx");
   delay(1000);
+  Serial.println(""); // Sacrificial Data since Arduino (or esp32) sends the data so badly, that it very often just sends the very first character, pauses for no reason and then sends the rest. This causes a receiver to receive the data in at least 2 lines
+  Serial.print("lux: ");
+  Serial.println(lux);
 }
